2bb5162bf8d5bf5ac8238109bd984afccd415ac0
laurenf720 <laurenf720@gmail.com>
2015-10-07 19:05:48 -0700

    preliminary TCP stuff
    testing connection. run server.py somewhere, then run vpn.py. as
    client, enter the necessary info. click send. currently not using the
    chat panel or getting text input from chat panel. currently need to
    press connect again after sending message to send another (don’t know
    why)

---
+++ client.py
@@ -0,0 +1,10 @@
+from socket import *
+serverName = '192.168.1.75'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(sentence.encode('utf-8'))
+modifiedSentence = clientSocket.recv(1024)
+print ('From Server:', modifiedSentence)
+clientSocket.close()
--- vpn.py
+++ vpn.py
@@ -10,21 +10,19 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.button import Button
 
-
-def SettingsEntry(text=None):
-        boxlayout = BoxLayout(orientation="vertical",
-                              padding=30)
-        label = Label(text=text,
-                      size=(300, 50),
-                      size_hint=(1, None))
-        boxlayout.add_widget(label)
-        textinput = TextInput(multiline=False, 
-                              size=(300, 50), 
-                              size_hint=(1, None))
-        boxlayout.add_widget(textinput)
-        return boxlayout
-
-
+from socket import *
+'''
+def connect_fnc(self):
+    serverName = '192.168.1.75'
+    serverPort = 12000
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName,serverPort))
+    sentence = 'hello'
+    clientSocket.send(sentence.encode('utf-8'))
+    modifiedSentence = clientSocket.recv(1024)
+    print ('From Server:', modifiedSentence)
+    clientSocket.close()
+'''
 class ColoredBoxLayout(BoxLayout):
 
     def __init__(self, background_color=(160,160,160,0.5), **kwargs):
@@ -43,6 +41,50 @@ class ColoredBoxLayout(BoxLayout):
 
 
 class VpnApp(App):
+    def SettingsEntry(self,text=None):
+        boxlayout = BoxLayout(orientation="vertical", padding=30)
+        self.label = Label(text=text, size=(300, 50),size_hint=(1, None))
+        boxlayout.add_widget(self.label)
+        self.textinput = TextInput(multiline=False, size=(300, 50), size_hint=(1, None))
+        boxlayout.add_widget(self.textinput)
+        return boxlayout
+    
+    def connect_fnc(self, btn):
+        serverPort = int(self.port_textinput.text)
+        if(self.clientmode.state=='down'):
+            serverName = self.ip_textinput.text
+            self.clientSocket = socket(AF_INET, SOCK_STREAM)
+            self.clientSocket.connect((serverName,serverPort))
+            '''sentence = 'hello'
+            self.clientSocket.send(sentence.encode('utf-8'))
+            modifiedSentence = self.clientSocket.recv(1024)
+            print ('From Server:', modifiedSentence)
+            self.clientSocket.close()'''
+        else:
+            self.serverSocket = socket(AF_INET,SOCK_STREAM)
+            self.serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+            self.serverSocket.bind(('',serverPort))
+            self.serverSocket.listen(1)
+            print ('The server is ready to receive')
+            '''waits until client initiates TCP connection'''
+            while (1):
+                self.connectionSocket, addr = self.serverSocket.accept()
+                sentence = self.connectionSocket.recv(1024)
+                capitalizedSentence = sentence.decode('utf-8').upper()
+                print(capitalizedSentence)
+                self.connectionSocket.send(capitalizedSentence.encode('utf-8'))
+                self.connectionSocket.close()
+                self.serverSocket.close()
+                break;
+
+    def disconnect_fnc(self, btn):
+        pass
+    
+    def send_msg(self,btn):
+        message = 'hello'
+        self.clientSocket.send(message.encode('utf-8'))
+        modifiedSentence = self.clientSocket.recv(1024)
+        print ('From Server:', modifiedSentence)
 
     def build(self):
         root = BoxLayout(orientation="horizontal",
@@ -56,22 +98,37 @@ class VpnApp(App):
                                         padding=10)
         root.add_widget(settings_panel)
 
-        client = ToggleButton(text='Client', group='mode', state='down')
-        server = ToggleButton(text='Server', group='mode')
-        settings_panel.add_widget(client)
-        settings_panel.add_widget(server)
+        self.clientmode = ToggleButton(text='Client', group='mode', state='down')
+        self.servermode = ToggleButton(text='Server', group='mode')
+        settings_panel.add_widget(self.clientmode)
+        settings_panel.add_widget(self.servermode)
 
-        ip_address = SettingsEntry(text="VPN Server IP Address")
+        '''ip_address = self.SettingsEntry(text="VPN Server IP Address")
+        settings_panel.add_widget(ip_address)'''
+        ip_address = BoxLayout(orientation="vertical", padding=30)
+        self.ip_label = Label(text="VPN Server IP Address", size=(300, 50),size_hint=(1, None))
+        ip_address.add_widget(self.ip_label)
+        self.ip_textinput = TextInput(multiline=False, size=(300, 50), size_hint=(1, None))
+        ip_address.add_widget(self.ip_textinput)
         settings_panel.add_widget(ip_address)
 
-        port = SettingsEntry(text="VPN Server Port")
+        '''port = self.SettingsEntry(text="VPN Server Port")
+        settings_panel.add_widget(port)'''
+        port = BoxLayout(orientation="vertical", padding=30)
+        self.port_label = Label(text="VPN Server Port", size=(300, 50),size_hint=(1, None))
+        port.add_widget(self.port_label)
+        self.port_textinput = TextInput(multiline=False, size=(300, 50), size_hint=(1, None))
+        port.add_widget(self.port_textinput)
         settings_panel.add_widget(port)
+        
 
-        shared_secret_value = SettingsEntry(text="Shared Secret Value")
+        shared_secret_value = self.SettingsEntry(text="Shared Secret Value")
         settings_panel.add_widget(shared_secret_value)
 
         connect = Button(text="Connect", color=(0,1,0))
+        connect.bind(on_press=self.connect_fnc)
         disconnect = Button(text="Disconnect", color=(1,0,0))
+        disconnect.bind(on_press=self.disconnect_fnc)
         settings_panel.add_widget(connect)
         settings_panel.add_widget(disconnect)
 
@@ -79,23 +136,24 @@ class VpnApp(App):
         settings_panel.add_widget(empty_widget)
 
         # create chat panel
-        chat_layout = BoxLayout(orientation="vertical",
+        self.chat_layout = BoxLayout(orientation="vertical",
                                 spacing=10,
                                 size_hint=(0.7, 1))
-        chat_panel = TextInput(readonly=True, 
+        self.chat_panel = TextInput(readonly=True,
                                disabled=True)
-        input_layout = BoxLayout(orientation="horizontal",
+        self.input_layout = BoxLayout(orientation="horizontal",
                                  spacing=10,
                                  size=(0, 50),
                                  size_hint=(1, None))
-        chat_input = TextInput(size_hint=(0.8, 1))
-        input_button = Button(size_hint=(0.2, 1),
+        self.chat_input = TextInput(size_hint=(0.8, 1))
+        self.input_button = Button(size_hint=(0.2, 1),
                               text="Send")
-        input_layout.add_widget(chat_input)
-        input_layout.add_widget(input_button)
-        chat_layout.add_widget(chat_panel)
-        chat_layout.add_widget(input_layout)
-        root.add_widget(chat_layout)
+        self.input_button.bind(on_press=self.send_msg)
+        self.input_layout.add_widget(self.chat_input)
+        self.input_layout.add_widget(self.input_button)
+        self.chat_layout.add_widget(self.chat_panel)
+        self.chat_layout.add_widget(self.input_layout)
+        root.add_widget(self.chat_layout)
         return root
 
 if __name__ == "__main__":
