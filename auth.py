2da6364d7913a8fe2c2ec37d7b77eeba9152adcc
kwou <kdw.5@hotmail.com>
2015-10-15 22:40:49 -0700

    Formatted auth.py

--- auth.py
+++ auth.py
@@ -5,9 +5,9 @@ from Crypto.Cipher import AES
 
 import TCPconnection
 
-class Authentication(object):
 
-    #These values are public so they can be hardcoded
+class Authentication(object):
+    # These values are public so they can be hardcoded
     shared_prime = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497C515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
     shared_base = 2;
 
@@ -29,16 +29,16 @@ class Authentication(object):
         return plaintext
 
     def mutualauth(self, machine):
-        if(machine == "server"):
+        if (machine == "server"):
             #Wait for client to reach out
-            response = TCP.listen() #TODO: OR whatever the listen call is
+            response = TCP.listen()  #TODO: OR whatever the listen call is
 
             #Client response is in the form: ["thisisclient,Ranonce"]
             split_resp = response.split(',')
             filler = split_resp[0]
             Ranonce = split_resp[1]
 
-            if(filler!="thisisclient"):
+            if (filler != "thisisclient"):
                 print('Initial Client message is not in the correct format. Received {}'.format(response))
                 print('Mutual Authentication failed')
                 return False
@@ -46,14 +46,16 @@ class Authentication(object):
             #Server response is in the form : ["Rbnonce,E("server",Ranonce,(g^b)modp)]
             Rbnonce = uuid.uuid4()
             b = random.getrandbits(2048)
-            gbmodp = pow(g,b,p)
+            gbmodp = pow(g, b, p)
             serv_resp = "server," + Ranonce + "," + gbmodp
-            encr_serv_resp = encryptmessage(serv_resp, self.sharedKey) #TODO Run serv_resp through Aes in cbc mode using shared key
-            TCPcon.send("" + Rbnonce + "," + encr_serv_resp) #TODO Actually send it
+            encr_serv_resp = encryptmessage(serv_resp,
+                                            self.sharedKey)  #TODO Run serv_resp through Aes in cbc mode using shared key
+            TCPcon.send("" + Rbnonce + "," + encr_serv_resp)  #TODO Actually send it
 
             #Wait for client's encrypted message             
             encr_client_resp = TCP.listen()
-            decr_client_resp = decryptmessage(encr_client_resp, self.sharedKey) #TODO: Decrypt encr_client_resp through AES in cbc mode
+            decr_client_resp = decryptmessage(encr_client_resp,
+                                              self.sharedKey)  #TODO: Decrypt encr_client_resp through AES in cbc mode
 
             #which is in the form: ["E("client",Rbnonce,(g^a)modp)"]
             split_resp = decr_client_resp.split(',')
@@ -61,20 +63,20 @@ class Authentication(object):
             received_nonce = split_resp[1]
             gamodp = split_resp[2]
 
-            if(filler!="client" or received_nonce!=Rbnonce):
+            if (filler != "client" or received_nonce != Rbnonce):
                 print('Encrypted message from client is not correct. Mutual Authentication Failed')
-		return False
-            
-            self.sessionKey = pow(gamodp,b,p)
+                return False
+
+            self.sessionKey = pow(gamodp, b, p)
             #At this point, we can be sure that we are talking with the correct client
             #and we have a shared session key
             return True;
 
-        elif(machine == "client"):
+        elif (machine == "client"):
             #Generate a nonce and send this to the server
             #Initiate contact by sending the following message: ["thisisclient,Ranonce"]
             Ranonce = uuid.uuid4().int
-            TCPcon.send("thisisclient," + Ranonce);#TODO: Change this to however we are sending the message
+            TCPcon.send("thisisclient," + Ranonce);  #TODO: Change this to however we are sending the message
 
             #Wait for the server response
             #In the form: ["Rbnonce,E("server",Ranonce,(g^b)modp)"]
@@ -85,27 +87,29 @@ class Authentication(object):
             Rbnonce = split_resp[0]
             encr_server_resp = split_resp[1]
 
-            decr_server_resp = decryptmessage(encr_server_resp, self.sharedKey) #TODO: Decrypt this using Aes in cbc mode
-            
+            decr_server_resp = decryptmessage(encr_server_resp,
+                                              self.sharedKey)  #TODO: Decrypt this using Aes in cbc mode
+
             #Split the decrypted message to get the filler, client nonce, and (g^b)modp
             split_resp = decr_server_resp.split(',')
             filler = split_resp[0]
             nonce = split_resp[1]
             gbmodp = split_resp[2]
 
-            if(nonce!=Ranonce or filler!=server):
+            if (nonce != Ranonce or filler != server):
                 print('Client is not talking to authorized server. Mutual Authentication failed')
                 return False
 
             #Client responds in the form: ["E(client,Rbnonce,(g^a)modp)"]
             a = random.getrandbits(2048)
-            gamodp = pow(g,b,p)
-            encr_client_resp = encryptmessage("client,"+Rbnonce + "," + gamodp, self.sharedKey)#TODO Encrpt this using Aes in cbc mode
+            gamodp = pow(g, b, p)
+            encr_client_resp = encryptmessage("client," + Rbnonce + "," + gamodp,
+                                              self.sharedKey)  #TODO Encrpt this using Aes in cbc mode
             TCPconn.send(encr_client_resp)
 
             #Calculate the session key
-            self.sessionKey = pow(gbmodp,a,p)
-            
+            self.sessionKey = pow(gbmodp, a, p)
+
             #We are now guaranteed to be talking with our server
             #We also now have a shared session key
             return True
